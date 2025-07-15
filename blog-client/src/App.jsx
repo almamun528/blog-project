4645b95b4fd54f8ebbd238e3520cdab1278b01c3
almamun528 <ayapths@gmail.com>
2025-07-16 01:01:13 +0600

    setup project

---
+++ blog-client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <section className='bg-red-500 text-center py-3 text-white '>
+      App is running...
+    </section>
+  )
+}
+
+export default App
